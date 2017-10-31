6f6eb049c23c26aa641aac66b6b18206161b5f5d
Jonas <N/A>
2017-10-31 18:44:58 +0100

    Adding the BookDbContext Class

--- Bookshelf/Data/BookDbContext.cs
+++ Bookshelf/Data/BookDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bookshelf.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,15 @@ namespace Bookshelf.Data
 {
     public class BookDbContext : DbContext 
     {
+        public DbSet<Book> Books { get; set; }
+
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
