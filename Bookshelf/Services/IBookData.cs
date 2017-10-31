3d9262d3e0bd6af992dbbff3da64ccb9b1b140d7
Jonas <N/A>
2017-10-31 19:40:32 +0100

    Adding the IBookData Service Interface

---
+++ Bookshelf/Services/IBookData.cs
@@ -0,0 +1,16 @@
+﻿using Bookshelf.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookshelf.Services
+{
+    public interface IBookData
+    {
+        IEnumerable<Book> GetAll();
+        Book Get(int id);
+        void Add(Book newBook);
+        int Commit();
+    }
+}
