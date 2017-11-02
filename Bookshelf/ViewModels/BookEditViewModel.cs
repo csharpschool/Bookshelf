a2d00413712f4eda01730183caedb1a8a1374c94
Jonas <N/A>
2017-11-02 13:48:39 +0100

    Adding the BookEditViewModel Class

---
+++ Bookshelf/ViewModels/BookEditViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Bookshelf.Models;
+
+namespace Bookshelf.ViewModels
+{
+    public class BookEditViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Genres Genre { get; set; }
+    }
+}
