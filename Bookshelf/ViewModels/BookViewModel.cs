9053b9681b0d34ffc3705991e07a6b1997b64cd6
Jonas <N/A>
2017-11-02 11:36:05 +0100

    Adding the View Model

---
+++ Bookshelf/ViewModels/BookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookshelf.ViewModels
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+    }
+}
